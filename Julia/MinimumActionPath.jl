48eb44c588ba165dcbe3f426c2727931a8ebd278
Rowan Brackston <rowan.brackston@cantab.net>
2017-11-07 16:54:43 +0000

    Script to calculate MAP

---
+++ Julia/MinimumActionPath.jl
@@ -0,0 +1,16 @@
+using Optim
+
+# Specify the SDE
+
+# f(x) = ...
+# g(x) = ...
+
+N = 100;    # Number of discrete points
+T = 100.0;  # Time span
+# x₀ = [];  # Start point
+# xₑ = [];  # End point
+# xᵢ = [];  # Intermediate point
+
+# Calculate the discretised piecewise path between the points
+
+# Use Optim to optimise over path φ
