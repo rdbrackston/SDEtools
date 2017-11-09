f738197b47e7a45de6a0e13f63a560fca35cb235
Rowan Brackston <rowan.brackston@cantab.net>
2017-11-09 16:50:18 +0000

    Functorised MAP

--- Julia/MinimumActionPath.jl
+++ Julia/MinimumActionPath.jl
@@ -1,6 +1,6 @@
 using Optim
-# using ForwardDiff
-using ReverseDiff
+using ForwardDiff
+# using ReverseDiff
 using Gadfly
 
 # Specify the SDE
@@ -13,44 +13,56 @@ using Gadfly
 f(x) = [2α*x[1]-4λ*x[1]^3-β; -4λ*x[2]^3];
 g(x) = σ*[1.0; 1.0];
 
-N = 101;    # Number of discrete points
 T = 100.0;  # Time span
-dT = T/N;
-x₀ = [-1.0; 0.0];  # Start point
-xₑ = [1.0; 0.0];  # End point
+x₀ = [-1.2; 0.0];  # Start point
+xₑ = [1.2; 0.0];  # End point
 # xᵢ = [0.0; 0.0];  # Intermediate point
 
-# Calculate the discretised piecewise path between the points
-φ₀ = vcat(collect(linspace(x₀[1],xₑ[1],N))',collect(linspace(x₀[2],xₑ[2],N))');
-φ₀ = φ₀[:,2:end-1];
+function MAP(f::Function, g::Function, x₀::Vector, xₑ::Vector, T::Real)
 
-# Define the action functional
-function S(φ::AbstractArray)
+    N = 101;    # Number of discrete points
+    dT = T/N;
 
-    V = hcat(x₀,φ,xₑ);
-    # print(size(V))
-    act = 0.0;
+    # Calculate the discretised piecewise path between the points
+    φ₀ = vcat(collect(linspace(x₀[1],xₑ[1],N))',collect(linspace(x₀[2],xₑ[2],N))');
+    φ₀ = φ₀[:,2:end-1];
 
-    for iT = 1:N-1
+    # Define the action functional
+    function S(φ::AbstractArray)
 
-        xT = 0.5*(V[:,iT+1]-V[:,iT]);
-        fT = f(xT);
-        gT = g(xT);
+        V = hcat(x₀,φ,xₑ);
+        # V = φ;
 
-        for ii = 1:size(V)[1]
-            dV = (V[ii,iT+1]-V[ii,iT])/dT;
-            act += dT*(dV-fT[ii])^2/gT[ii]^2;
+        action = 0.0;
+
+        for iT = 1:N-1
+
+            xT = 0.5*(V[:,iT+1]-V[:,iT]);
+            fT = f(xT);
+            gT = g(xT);
+
+            for ii = 1:size(V)[1]
+                dV = (V[ii,iT+1]-V[ii,iT])/dT;
+                action += dT*(dV-fT[ii])^2/gT[ii]^2;
+            end
         end
+
+        return 0.5*action
+
     end
 
-    return 0.5*act
+    store = zeros(φ₀);
+    dS!(store,φ) = ForwardDiff.gradient!(store, S, φ);
+
+    return Optim.optimize(S, dS!, φ₀, LBFGS());
 
 end
 
-store = zeros(φ₀);
-# dS!(store,φ) = ForwardDiff.gradient!(store, S, φ);
-dS!(store,φ) = ReverseDiff.gradient!(store, S, φ);
+
+
+# dS!(store,φ) = ReverseDiff.gradient!(store, S, φ);
 
 # Use Optim to optimise over path φ
-resObj = Optim.optimize(S, dS!, φ₀, LBFGS());
-res = Optim.minimizer(resObj);
+resObj = MAP(f, g, x₀, xₑ, T);
+res = Optim.minimizer(resObj)
+val = Optim.minimum(resObj)
