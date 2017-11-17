f9cc49fa0740a06674a0481c52294fd9c23eb85d
Rowan Brackston <rowan.brackston@cantab.net>
2017-11-17 15:50:05 +0000

    Added data files

--- Julia/ChickarmaneFwd.jl
+++ Julia/ChickarmaneFwd.jl
@@ -12,9 +12,12 @@ N = 150;
 
 # Perform the optimisation
 Tvec = [];    Svec = [];
-resObj = MAP.T_Opt!(Tvec,Svec, f,g,xₛ,xₑ,(500.,5000.),N)
-res = Optim.minimizer(resObj)
-plt = plot(Tvec,Svec)
+resObj = MAP.T_Opt!(Tvec,Svec, f,g,xₛ,xₑ,(100.,4000.),N)
+res = vcat(xₛ,Optim.minimizer(resObj),xₑ);
 
-resFull = hcat(collect(linspace(0,Tvec[end],N-2)),resrev);
+plt = plot(Tvec,Svec, line=0,marker=2)
+plt = plot(res[:,4],res[:,1], line=0,marker=4)
+
+
+resFull = hcat(collect(linspace(0,Tvec[end],N)),res);
 writedlm("ChickFwdPth.txt",resFull);
--- Julia/ChickarmaneRev.jl
+++ Julia/ChickarmaneRev.jl
@@ -12,9 +12,12 @@ N = 150;
 
 # Perform the optimisation
 Tvec = [];    Svec = [];
-resObj = MAP.T_Opt!(Tvec,Svec, f,g,xₑ,xₛ,(100.,2000.),N)
-res = Optim.minimizer(resObj)
-plt = plot(Tvec,Svec)
+resObj = MAP.T_Opt!(Tvec,Svec, f,g,xₑ,xₛ,(100.,4000.),N)
+res = vcat(xₑ,Optim.minimizer(resObj),xₛ);
 
-resFull = hcat(collect(linspace(0,Tvec[end],N-2)),resrev);
-writedlm("ChickFwdPth.txt",resFull);
+plt = plot(Tvec,Svec, line=0,marker=2)
+plt = plot(res[:,4],res[:,1], line=0,marker=4)
+
+
+resFull = hcat(collect(linspace(0,Tvec[end],N)),res);
+writedlm("ChickRevPth.txt",resFull);
